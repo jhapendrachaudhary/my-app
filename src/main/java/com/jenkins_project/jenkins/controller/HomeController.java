a4078717f3eacdb777cb7abbbae26ed5751fe129
Jhapendra <theunknownmen777@gmail.com>
2025-09-03 13:59:53 +0545

    final commit

--- src/main/java/com/jenkins_project/jenkins/controller/HomeController.java
+++ src/main/java/com/jenkins_project/jenkins/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String Greeting(){
         return "Hello Buddy <br> <h1>Welcome to my app....</h1>";
     }
